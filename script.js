d43f81107b83bd16d744a6a80a9cb9fff78e2e25
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-01-30 20:20:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const ob1={
+    name:"Rohan",
+    lastname:"Patil",
+}
+
+const ob2={
+    email:"rohanpatil1797@gmmail.com",
+    
+}
+
+let name="Rohan   ";
+String.prototype.truelength=function()
+{
+    console.log(`truelength is ${this.trim().length}`);
+}
+
+Object.prototype.Rohan=function()
+{
+
+}
+
+let arr1=['a','b','c'];
+
+
