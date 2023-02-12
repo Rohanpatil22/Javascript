17319875f3e1775a791368fc8aa3d46a5a21a5b9
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-02-12 19:58:15 +0530

    assignment 4 completed

---
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/solution.js
@@ -0,0 +1,24 @@
+
+function page_load(){
+
+   let ele=document.querySelectorAll(".clearfix");
+console.log(ele);
+let ele_arr=[...ele];
+console.log(ele_arr);
+ele[0].style.backgroundColor="#ec9b3b";
+ele[1].style.backgroundColor="#ee548f";
+ele[2].style.backgroundColor="#f6901a";
+ele[3].style.backgroundColor="#82bb30";
+ele[4].style.backgroundColor="#4facff";
+
+let sup_txt=document.querySelectorAll(".stat");
+let sup_txt_arr=[...sup_txt];
+
+let app_color=sup_txt_arr.map((value)=>{value.style.color="white"});
+
+let sup_txt_2=document.querySelectorAll(".stat-value");
+let sup_txt_2_arr=[...sup_txt_2];
+
+let app_color_2=sup_txt_2_arr.map((ele)=>{ele.style.color="white"});
+   
+}
